459197a1d52f2b6c8db1d06f3a3f40b0ae735247
marek.hirschner <hirschner.marek@gmail.com>
2023-05-12 21:43:34 +0200

    Added changes in LatexController

--- app/Http/Controllers/LatexController.php
+++ app/Http/Controllers/LatexController.php
@@ -12,14 +12,14 @@ use Symfony\Component\Process\Process;
 
 class LatexController extends Controller
 {
+    private LatexParseService $parseService;
 
     public function extractData(Request $request, $fileName) {
+        $parseService = app(LatexParseService::class);
 
-        // Read the contents of the LaTeX file
-        // $file_contents = file_get_contents('../public/LatexFiles/blokovka01pr.tex');
-        $file_path = '../public/LatexFiles/' . $file_name . '.tex';
+        $file_path = base_path('public/LatexFiles/') . $fileName . '.tex';
 
-        $process = new Process(['python', '../../resources/python/latexParser.py', $file_path]);
+        $process = new Process(['python3', base_path('python_files/latexParser.py'), $file_path]);
         $process->run();
 
 
