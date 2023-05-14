6f2d90f539afa88030314cb74209b9086fb2fefe
halgi <xhalgas@stuba.sk>
2023-05-14 13:30:44 +0200

    Change Languague working button

---
+++ app/Http/Controllers/LangController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class LangController extends Controller
+{
+    public function changeLang(){
+        if (session('locale') == "sk"){
+            session(['locale' => "en"]);
+        } else {
+            session(['locale' => "sk"]);
+        }
+        return redirect()->back();
+    }
+
+    public function returnLang($lang){
+        App::setLocale($lang);
+        return view('welcome');
+    }
+}
