34d14d81c6fbfdf05935ab0f5ebc8caf14c3bc77
xmydliarm <xmydliarm1@stuba.sk>
2023-05-16 18:12:27 +0200

    bugfix

--- python_files/equationComparator.py
+++ python_files/equationComparator.py
@@ -3,11 +3,10 @@ from sympy import simplify
 from sympy.parsing.latex import parse_latex
 
 # Get a latex_string arguments from the command line
-latex_string = r"{}".format(sys.argv[1])
-latex_string2 = r"{}".format(sys.argv[2])
+equation = sys.argv[1]
+latex_string2 = sys.argv[2]
 
 # Parse the latex_string into a sympy equations
-equation = parse_latex(latex_string)
 equation2 = parse_latex(latex_string2)
 
 # Simplify the equations
@@ -15,6 +14,6 @@ simplified_equation1 = simplify(equation)
 simplified_equation2 = simplify(equation2)
 
 if simplified_equation1 == simplified_equation2:
-    print("false")
-else:
     print("true")
+else:
+    print("false")
