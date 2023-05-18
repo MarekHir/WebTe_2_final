27c79eed62f304f7c401767d6b20482738dfec04
marek.hirschner <hirschner.marek@gmail.com>
2023-05-18 13:50:17 +0200

    Final instructions modifications

--- app/Models/Instructions.php
+++ app/Models/Instructions.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\Auth;
 
 class Instructions extends Model
@@ -21,6 +23,29 @@ class Instructions extends Model
         static::saving(function ($model) {
             $model->updated_by = Auth::id();
         });
+    }
+
+    public function created_by(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function updated_by(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'updated_by');
+    }
+
+    public function scopeWithUsers(Builder $query): void
+    {
+        $query->with(['created_by', 'updated_by']);
+    }
+
+    public function scopeForRole(Builder $query, string $role): void
+    {
+        if ($role === 'student') {
+            $query->where('for_user_type', '=', 'student', 'or')
+                ->where('for_user_type', '=', 'all');
+        }
     }
 
     public function isForStudents(): bool
